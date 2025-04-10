2ca16e12957c592e1e40d6c5152b833742815bac
FA <fa@fa.local>
2025-04-11 00:51:32 +0330

    Prepare for Dokploy deployment 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c@8-9n^hm1$zo9yfjqy3nb7m*)v=3wjbd&al7n_((2ck#a7xib
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']  # Allow all hosts for Dokploy
+ALLOWED_HOSTS = ['teljoo.com', 'www.teljoo.com', 'localhost', '127.0.0.1']
 
 # Security settings
 SECURE_SSL_REDIRECT = True
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
